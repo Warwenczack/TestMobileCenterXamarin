362d7f202e4416038b53a0c96e6969de197520b9
William <williamcardosowarwenczack@gmail.com>
2017-10-04 09:05:09 -0300

    Inicio do projeto

---
+++ CustomBottomMenu/CustomBottomMenu/App.cs
@@ -0,0 +1,32 @@
+﻿using CustomBottomMenu.Pages;
+using Xamarin.Forms;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+
+namespace CustomBottomMenu
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            MobileCenter.Start("android=f9750e5c-1cb3-460b-bcac-7af29712a397;" +
+                   "uwp={Your UWP App secret here};" +
+                   "ios={Your iOS App secret here}",
+                   typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
